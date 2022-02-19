fe55478e7a3340086d333ac6d2c0edb27b8503b4
Evan Rispoli <evan.rispoli@gmail.com>
2022-02-19 19:16:23 -0300

    Configuracao de ambiente Criacao de model criacao de classe da entidade criacao da classe context

---
+++ GerenciamentoUsuarios/Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciamentoUsuarios.Models
+{
+    [Table("Usuarios")]
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required,MaxLength(120)]
+        public string Name { get; set; }
+        [Required, MaxLength(50)]
+        public string Email { get; set; }
+        [Required, MaxLength(10)]
+        public string Senha { get; set; }
+        [Required, MinLength(11),MaxLength(11)]
+        public string Cpf { get; set; }
+    }
+}
