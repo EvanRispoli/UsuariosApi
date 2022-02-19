a64e093422a2a1989643b24933e729000061cd8c
Evan Rispoli <evan.rispoli@gmail.com>
2022-02-19 14:21:01 -0300

    Criacao do diretorio Context e classe UsuarioContext

---
+++ GerenciamentoUsuariosApi/Context/UsuarioContext.cs
@@ -0,0 +1,15 @@
+﻿using GerenciamentoUsuariosApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoUsuariosApi.Context
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options)
+            : base(options)=> Database.EnsureCreated();
+
+        public Usuario Usuario { get; set; }
+
+
+    }
+}
